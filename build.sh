13958c1bc61dab916f774d4433a26768174b8a8b
test <test>
2020-01-05 13:10:32 +0800

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #bin/bash
-GOOS=windows GOARCH=amd64 go build -o go-composer_win_amd64.exe
-GOOS=darwin GOARCH=amd64 go build -o go-composer_darwin_amd64
-GOOS=linux GOARCH=amd64 go build -o go-composer_linux_amd64
+GOOS=windows GOARCH=amd64 go build -o go-composer_win_amd64_beta-0.0.2.exe
+GOOS=darwin GOARCH=amd64 go build -o go-composer_darwin_amd64_beta-0.0.2
+GOOS=linux GOARCH=amd64 go build -o go-composer_linux_amd64_beta-0.0.2
