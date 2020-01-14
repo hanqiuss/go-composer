d031fadb5c0edbc9a85cb5dfca6a0c1f925331fd
renyk <renyk@rntd.cn>
2020-01-14 09:19:53 +0800

    build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #bin/bash
-GOOS=windows GOARCH=amd64 go build -o build/go-composer_win_amd64_beta-0.0.2.exe
-GOOS=darwin GOARCH=amd64 go build -o build/go-composer_darwin_amd64_beta-0.0.2
-GOOS=linux GOARCH=amd64 go build -o build/go-composer_linux_amd64_beta-0.0.2
+GOOS=windows GOARCH=amd64 go build -o build/go-composer_win_amd64_beta_0.0.3.exe
+GOOS=darwin GOARCH=amd64 go build -o build/go-composer_darwin_amd64_beta-0.0.3
+GOOS=linux GOARCH=amd64 go build -o build/go-composer_linux_amd64_beta-0.0.3
